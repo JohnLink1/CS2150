64e25f10d56433b75ac14974a0a5b5955a4d664d
John Link <John Link>
2021-09-17 14:54:09 -0400

    half way done with prelab 4

--- Lab4/prelab4.cpp
+++ Lab4/prelab4.cpp
@@ -1,19 +1,57 @@
 //John Link, jwl9vq@virginia.edu, 9/17/21, prelab4.cpp
 
+#include <iostream>
+#include <cmath>
+#include <string>
 
-sizeOfTest(){
+using namespace std;
+
+void sizeOfTest(){
 
 }
 
-overflow(){
-
+void overflow(){
+unsigned int power = 0;
+for(int x = 0; x < 32; x++){
+    power += pow(2, x);
+}
+unsigned int over = power  + 1;
+cout << power << " + 1 = " << over << endl;
 }
 
-outputBinary(int x){
 
+string outputBinary(unsigned int x){
+string str = "";
+int bit = 31;
+unsigned int power;
+int divis;
+while(str.length() < 32){
+    power = pow(2, bit);
+    divis = x / power;
+    if(divis == 1){
+        str += "1";
+        x = x % power;
+    }
+    else{
+        str += "0";
+    }
+    bit --;
+}
+return str;
 }
 
 int main() {
+
+int input = 0;
+cin >> input;
+
+
+overflow();
+string out = outputBinary(input);
+for(int x = 4; x < out.length() - 1; x += 5){
+    out = out.substr(0, x) + " " + out.substr(x);
+}
+cout << out << endl;
 
 
 return 0;

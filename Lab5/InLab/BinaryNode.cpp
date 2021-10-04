ae7245da17ed29c772b28283e0c4d27916ac3d37
John Link <John Link>
2021-10-04 11:28:10 -0400

    added inlab files

---
+++ Lab5/InLab/BinaryNode.cpp
@@ -0,0 +1,17 @@
+//John Link, jwl9vq@virginia.edu, 10/04/2001, BinaryNode.cpp
+#include "BinaryNode.h"
+#include <string>
+using namespace std;
+
+BinaryNode::BinaryNode() {
+    value = "?";
+    left = NULL;
+    right = NULL;
+}
+
+BinaryNode::~BinaryNode() {
+    delete left;
+    delete right;
+    left = NULL;
+    right = NULL;
+}

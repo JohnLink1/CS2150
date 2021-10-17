a5967751f5f77e8deb5e962ae8ff18a315c3c24e
John Link <john.link33@gmail.com>
2021-10-16 21:16:20 -0400

    added inlab

---
+++ Lab6/InLab/hashTable.cpp
@@ -0,0 +1,74 @@
+//John Link, jwl9vq@virginia.edu, 10/10/21, hashTable.cpp
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <cmath>
+#include "hashTable.h"
+
+using namespace std;
+
+HashTable::HashTable() : num_ele(1009){
+    table = new vector<string>(num_ele);
+    for(int x = 0; x < 30; x++){
+        offsets[x] = (unsigned int)pow(37, x);
+    }
+    for(int x = 0; x < num_ele; x++){
+        table->push_back("");
+    }
+}
+
+HashTable::HashTable(int x){
+    num_ele = x;
+    table = new vector<string>(num_ele);
+    for(int x = 0; x < 30; x++){
+        offsets[x] = (unsigned int)pow(37, x);
+    }
+    for(int x = 0; x < num_ele; x++){
+        table->push_back("");
+    }
+}
+
+HashTable::~HashTable(){
+    delete table;
+    table = NULL;
+}
+
+void HashTable::insert(string value){
+    int h = hash(value);
+    table->at(h) = "," + value + "," + table->at(h);
+}
+
+bool HashTable::find(string value){
+    int h = hash(value);
+    if(table->at(h) != "" && table->at(h).find("," + value + ",") != string::npos){
+        return true;
+    }
+    return false;
+}
+
+string HashTable::retrieve(string value){
+    int h = hash(value);
+    if(find(value)){
+        string str = table->at(h);
+	return str.substr(str.find(value), str.length() + str.find(value));
+    }
+    return NULL;
+}
+
+int HashTable::hash(string value) const{
+    unsigned int k = 0;
+    for(int x = 0; x < value.length(); x++){
+        k += ((unsigned int)value.at(x)) * offsets[x];
+    }
+    return (int)(k % num_ele);
+}
+
+void HashTable::remove(string value){
+    int h = hash(value);
+    if(find(value)){
+        string str = table->at(h);
+        str = str.substr(0, str.find("," + value)) + str.substr(str.find(value) + str.length());
+        table->at(h) = str;
+    }
+}
---
+++ Lab6/InLab/hashTable.h
@@ -0,0 +1,29 @@
+//John Link, jwl9vq@virginia.edu, 10/10/21, hashTable.h
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+class HashTable {
+
+public:
+    HashTable();
+    HashTable(int x);
+    ~HashTable();
+    void insert(string value);
+    void remove(string value);
+    bool find(string value);
+    string retrieve(string value);
+
+private:
+    vector<string>* table;
+    unsigned int offsets[30];
+    int num_ele;
+    int hash(string value) const;
+
+};
+#endif

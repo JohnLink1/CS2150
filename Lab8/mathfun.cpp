6d5a54ed759bfb523b5dbeddf0c7c51e1d8a6db5
John Link <John Link>
2021-10-29 14:23:48 -0400

    need to make pow recursive

---
+++ Lab8/mathfun.cpp
@@ -0,0 +1,36 @@
+//John Link, jwl9vq@virginia.edu, 10/29/21, mathfun.cpp
+#include <iostream>
+#include <time.h>
+#include <cstdlib>
+
+using namespace std;
+
+extern "C" long multiply (long, long);
+extern "C" long power (long, long);
+
+int  main () {
+
+    // delcare the local variables
+    long  n1, n2, prod, pow;
+
+    // how big is the array we want to use?
+    cout << "Enter integer 1: ";
+    cin >> n1;
+    cout << "Enter integer 2: ";
+    cin >> n2;
+
+    // sanity check the array size
+    if (n1 < 0 && n2 < 0) {
+        cerr << "Numbers must be greater than zero.\n";
+        return 1;
+    }
+
+    // sum up the array and print out results
+    prod = multiply(n1, n2);
+    cout << "Product: " << prod << endl;
+
+    pow = power(n1, n2);
+    cout << "Power: " << pow << endl;
+
+    return 0;
+}
--- Lab8/mathlib.s
+++ Lab8/mathlib.s
@@ -1,21 +1,49 @@
 global mathlib
+extern multiply
+extern power
 section .data
 v1 DQ 0
 v2 DQ 0
+itr DQ 0
 prod DQ 0
 pow DQ 0
 
 section .text
+mathlib:
+mov [v1], rdi
+mov [v2], rsi
+xor r10, r10
+xor rax, rax
 
 multiply:
-add [prod], rdi
-cpm rsi, 0
-jeq endPgm
-sub rsi, 1
-jmp multiply
+mov [v1], rdi
+mov [v2], rsi
+xor r10, r10
+xor rax, rax
+loop:
+cmp rsi, r10
+je endloop
+add rax, rdi
+add r10, 1
+jmp loop
 
 power:
-mov [v]
+mov [v1], rdi
+mov [v2], rsi
+xor r10, r10
+xor rax, rax
+inc rax
+realPow:
+cmp rsi, r10
+je endloop
+mov [v2], rsi
+mov rsi, [v1]
+call multiply
+mov rsi, [v2]
+mov rdi, rax
+sub rsi, 1
+jmp realPow
 
-endPgrm:
+
+endloop:
 ret

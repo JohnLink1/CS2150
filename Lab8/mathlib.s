32736c29713b26b5d1826ad07806adf875924dba
John Link <john.link33@gmail.com>
2021-10-30 14:26:30 -0400

    finished prelab

--- Lab8/mathlib.s
+++ Lab8/mathlib.s
@@ -1,23 +1,11 @@
-global mathlib
-extern multiply
-extern power
+global multiply
+global power
 section .data
 v1 DQ 0
 v2 DQ 0
-itr DQ 0
-prod DQ 0
-pow DQ 0
 
 section .text
-mathlib:
-mov [v1], rdi
-mov [v2], rsi
-xor r10, r10
-xor rax, rax
-
 multiply:
-mov [v1], rdi
-mov [v2], rsi
 xor r10, r10
 xor rax, rax
 loop:
@@ -30,11 +18,12 @@ jmp loop
 power:
 mov [v1], rdi
 mov [v2], rsi
-xor r10, r10
+xor r11, r11
+inc r11
 xor rax, rax
 inc rax
 realPow:
-cmp rsi, r10
+cmp rsi, r11
 je endloop
 mov [v2], rsi
 mov rsi, [v1]
@@ -43,7 +32,6 @@ mov rsi, [v2]
 mov rdi, rax
 sub rsi, 1
 call realPow
-
 
 endloop:
 ret

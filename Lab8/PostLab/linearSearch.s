d5a6ae199745a58b9cec896180d4b14b76392353
John Link <john.link33@gmail.com>
2021-10-31 18:00:06 -0400

    finished postlab

---
+++ Lab8/PostLab/linearSearch.s
@@ -0,0 +1,22 @@
+global linearSearch
+section .text
+linearSearch:
+xor r10d, r10d
+xor r11d, r11d
+xor eax, eax
+loop:
+cmp r10d, esi
+je empty
+mov r11d, [rdi+4*r10]
+cmp edx, r11d
+je found
+inc r10d
+jmp loop
+
+found:
+mov eax, r10d
+ret
+
+empty:
+mov eax, -1
+ret

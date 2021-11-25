604c27a5fb10041b19a50368719af7c3924e8d70
John Link <John Link>
2021-11-25 11:09:04 -0500

    almost finished prelab

--- Lab11/PreLab/topological.cpp
+++ Lab11/PreLab/topological.cpp
@@ -1,10 +1,87 @@
 //John Link, jwl9vq@virginia.edu, 11/23/21, topological.cpp
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <fstream>
-
+#include <queue>
+#include <map>
+#include <stack>
+#include <list>
 
 using namespace std;
+
+class Node {
+    int size;
+    int var = 0;
+    map<string, int> idx;
+    list<string>* adj;
+    bool* visit;
+    void preSort(string str);
+
+public:
+    string first;
+    Node(int i);
+    void edge(string str1, string str2);
+    void sort();
+};
+
+Node::Node(int i){
+    size = i;
+    adj = new list<string>[i];
+    visit = new bool[i];
+}
+
+void Node::edge(string str1, string str2){
+    if(idx.count(str1) == 0){
+        idx.insert(pair<string, int>(str1, var));
+        var++;
+    }
+    if(idx.count(str2) == 0){
+        idx.insert(pair<string, int>(str2, var));
+        var++;
+    }
+    adj[idx[str1]].push_back(str2);
+    //cout << idx[str1] << " " << idx[str2] << endl;
+}
+
+void Node::preSort(string str){
+    visit[idx[str]] = true;
+
+    for(string s : adj[idx[str]]){
+        if(!visit[idx[s]]){
+            cout << s << " ";
+        }
+    }
+    for(string s : adj[idx[str]]){
+        if(!visit[idx[s]]){
+            preSort(s);
+        }
+    }
+}
+
+void Node::sort(){
+    for(int x = 0; x < size; x++){
+        visit[x] = false;
+    }
+    
+    for(pair<string, int> p : idx){
+        for(string s : adj[p.second]){
+            visit[idx[s]] = true;
+        }
+    }
+    for(pair<string, int> p : idx){
+        if(!visit[p.second])
+            first = p.first;
+    }
+
+    for(int x = 0; x < size; x++){
+        visit[x] = false;
+    } 
+    cout << first << " ";
+    preSort(first);
+
+    cout << endl;
+}
 
 int main(int argc, char** argv){
 
@@ -21,12 +98,28 @@ int main(int argc, char** argv){
         cout << "Unable to open '" << argv[1] << "' for reading" << endl;
         exit(2);
     }
+    string s1 = "";
+    string s2 = "";
+    queue<string> str1;
+    queue<string> str2;
+    int count = 0;
+    while(true){
+        file >> s1;
+        file >> s2;
+        if(s1 == "0" && s2 == "0")
+           break;
+        str1.push(s1);
+        str2.push(s2);
+        count++;
+    }
+    Node node(count);
+    for(int x = 0; x < count; x++){
+        node.edge(str1.front(), str2.front());
+        //cout << str1.front() << " " << str2.front() << endl;
+        str1.pop();
+        str2.pop();
+    }
     
-    string s1, s2;
-    file >> s1;
-    file >> s2;
-    
-    
-
+    node.sort();
     return 0;
 }
